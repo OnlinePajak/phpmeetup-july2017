30e7e1ebab06e2916517ef75b69cc3829b03f385
taufik <taufik@online-pajak.com>
2017-07-06 17:34:13 +0700

    lalala first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+meetup
+======
+
+A Symfony project created on July 3, 2017, 10:40 am.
---
+++ src/AppBundle/Entity/Cart.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Cart
+ *
+ * @ORM\Table(name="cart")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CartRepository")
+ */
+class Cart
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="item_id", type="integer")
+     */
+    private $itemId;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="amount", type="integer", nullable=true)
+     */
+    private $amount;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set itemId
+     *
+     * @param integer $itemId
+     *
+     * @return Cart
+     */
+    public function setItemId($itemId)
+    {
+        $this->itemId = $itemId;
+
+        return $this;
+    }
+
+    /**
+     * Get itemId
+     *
+     * @return int
+     */
+    public function getItemId()
+    {
+        return $this->itemId;
+    }
+
+    /**
+     * Set amount
+     *
+     * @param integer $amount
+     *
+     * @return Cart
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    /**
+     * Get amount
+     *
+     * @return int
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+}
+
