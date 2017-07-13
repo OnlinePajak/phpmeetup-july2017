1577245b557b74a2b2813e210b64729e42695e91
ardi <ardi.alam.wijaya@online-pajak.com>
2017-07-13 18:20:09 +0700

    implement ui

--- src/FrontEndBundle/Controller/DefaultController.php
+++ src/FrontEndBundle/Controller/DefaultController.php
@@ -2,12 +2,18 @@
 
 namespace FrontEndBundle\Controller;
 
+use Doctrine\Common\Util\Debug;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('FrontEndBundle:Default:index.html.twig');
+        $items = $this->get('meetup.api.item')->fetchAllItems();
+        Debug::dump($items);exit;
+        
+        return $this->render('FrontEndBundle:Default:index.html.twig' , array(
+            "items" => $items
+        ));
     }
 }
